52ce43abe324bcad8dd90c43244005fffc681b2d
Leon Huang <zlargon1988@gmail.com>
2022-12-27 11:23:27 -0500

    [middleware] basic auth

--- server.go
+++ server.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/zlargon/gograph/http"
+	"github.com/zlargon/gograph/middleware"
 )
 
 const defaultPort = "8080"
@@ -18,6 +19,7 @@ func main() {
 
 	// setup gin
 	server := gin.Default()
+	server.Use(middleware.BasicAuth())
 	server.GET("/", http.PlaygroundHandler())
 	server.POST("/query", http.GraphqlHandler())
 	server.Run(":" + port)
